25b90216bb108ef4f3a4108c6e67a01cf7382cb2
高洋 <879663394@qq.com>
2018-02-08 19:58:37 +0800

    删除

--- dailyfresh/apps/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import include, url
-from . import views
-
-urlpatterns = [
-    url(r'^register$', views.register, name='register'),
-    url(r'^handle_user$', views.handle_user, name='handle_user'),
-    url(r'^login$', views.login, name='login')
-]
