fa3d920a2b034a6c8906b79fa804e263eb940800
高洋 <879663394@qq.com>
2018-02-08 16:55:23 +0800

    立项

---
+++ dailyfresh/apps/orders/migrations/0001_initial.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='OrderGoods',
+            fields=[
+                ('id', models.AutoField(primary_key=True, serialize=False, auto_created=True, verbose_name='ID')),
+                ('create_time', models.DateTimeField(auto_now_add=True, verbose_name='创建时间')),
+                ('update_time', models.DateTimeField(auto_now=True, verbose_name='更新时间')),
+                ('count', models.IntegerField(verbose_name='数量', default=1)),
+                ('price', models.DecimalField(max_digits=10, decimal_places=2, verbose_name='单价')),
+                ('comment', models.TextField(verbose_name='评价信息', default='')),
+            ],
+            options={
+                'db_table': 'df_order_goods',
+            },
+        ),
+        migrations.CreateModel(
+            name='OrderInfo',
+            fields=[
+                ('create_time', models.DateTimeField(auto_now_add=True, verbose_name='创建时间')),
+                ('update_time', models.DateTimeField(auto_now=True, verbose_name='更新时间')),
+                ('order_id', models.CharField(primary_key=True, serialize=False, max_length=64, verbose_name='订单号')),
+                ('total_count', models.IntegerField(verbose_name='商品总数', default=1)),
+                ('total_amount', models.DecimalField(max_digits=10, decimal_places=2, verbose_name='商品总金额')),
+                ('trans_cost', models.DecimalField(max_digits=10, decimal_places=2, verbose_name='运费')),
+                ('pay_method', models.SmallIntegerField(verbose_name='支付方式', choices=[(1, '货到付款'), (2, '支付宝')], default=1)),
+                ('status', models.SmallIntegerField(verbose_name='订单状态', choices=[(1, '待支付'), (2, '待发货'), (3, '待收货'), (4, '待评价'), (5, '已完成')], default=1)),
+                ('trade_id', models.CharField(null=True, unique=True, blank=True, max_length=100, verbose_name='支付编号')),
+            ],
+            options={
+                'db_table': 'df_order_info',
+            },
+        ),
+    ]
